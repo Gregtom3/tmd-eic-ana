3f4c2d8f9cf2c1515ed8645b8a87de2dacab7196
Gregtom3 <gamatousek@gmail.com>
2025-09-09 14:10:48 -0400

    TCut loading

--- include/TMD.h
+++ include/TMD.h
@@ -3,8 +3,14 @@
 
 #include "TFile.h"
 #include "TTree.h"
+#include "Grid.h"
+#include "TCut.h"
+#include <map>
 
 // Function to load a ROOT file and TTree
 void loadRootFile(const char* filename, const char* treename);
+
+// Generate TCuts for each bin in a Grid (for X and Q)
+std::map<std::string, TCut> generateBinTCuts(const Grid& grid);
 
 #endif // TMD_H
--- src/TMD.cpp
+++ src/TMD.cpp
@@ -1,5 +1,8 @@
 #include "TMD.h"
 #include <iostream>
+#include "TCut.h"
+#include "Grid.h"
+#include <map>
 
 void loadRootFile(const char* filename, const char* treename) {
     // Open the ROOT file
@@ -22,3 +25,22 @@ void loadRootFile(const char* filename, const char* treename) {
     // Clean up
     file->Close();
 }
+
+std::map<std::string, TCut> generateBinTCuts(const Grid& grid) {
+    std::map<std::string, TCut> binTCuts;
+    const auto& bins = grid.getBins();
+    for (const auto& binPair : bins) {
+        const std::string& key = binPair.first;
+        const Bin& bin = binPair.second;
+        double X_min = bin.getMin("X");
+        double X_max = bin.getMax("X");
+        double Q_min = bin.getMin("Q");
+        double Q_max = bin.getMax("Q");
+        std::string cutStr =
+            "X >= " + std::to_string(X_min) + " && X < " + std::to_string(X_max) +
+            " && Q >= " + std::to_string(Q_min) + " && Q < " + std::to_string(Q_max);
+        binTCuts[key] = TCut(cutStr.c_str());
+    }
+    return binTCuts;
+}
+
